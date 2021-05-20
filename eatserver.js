dd8941d2a4403f198dbd0fb1d3c0890f73049719
zune2222 <zun_e@kakao.com>
2021-05-20 12:43:22 +0900

    0520-12:43

--- eatserver.js
+++ eatserver.js
@@ -7,12 +7,27 @@ app.use(express.static(path.join(__dirname, 'static')));
 app.get('/', (req, res) => {
   res.render(path.join(__dirname, 'static','html', 'home'));
 });
-app.get('/gallery', (req, res) => {
-  res.render(path.join(__dirname, 'static', 'html', 'gallery'));
+app.get('/koreanFood', (req, res) => {
+  res.render(path.join(__dirname, 'static', 'html', 'koreanFood'));
+});
+app.get('/meat', (req, res) => {
+  res.render(path.join(__dirname, 'static', 'html', 'meat'));
+});
+app.get('/other', (req, res) => {
+  res.render(path.join(__dirname, 'static', 'html', 'other'));
+});
+app.get('/ramen', (req, res) => {
+  res.render(path.join(__dirname, 'static', 'html', 'ramen'));
+});
+app.get('/americanFood', (req, res) => {
+  res.render(path.join(__dirname, 'static', 'html', 'americanFood'));
+});
+app.get('/chineseFood', (req, res) => {
+  res.render(path.join(__dirname, 'static', 'html', 'chineseFood'));
 });
 app.get('/login', (req, res) => {
   res.render(path.join(__dirname, 'static', 'html', 'login'));
 });
-app.listen(8080, () => {
-  console.log('Express App on port 8080!');
+app.listen(8090, () => {
+  console.log('Express App on port 8090!');
 });

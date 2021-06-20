50d81c9efe8f672baba580022e86cca9bc4962af
Ubuntu <ubuntu@ip-172-31-41-238.ap-northeast-2.compute.internal>
2021-06-20 12:23:35 +0000

    0620-21:23

--- eatserver.js
+++ eatserver.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'static')));
+const favicon = require('serve-favicon');
+app.use(favicon(path.join(__dirname,'static','favicon.ico')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'static','html', 'home.html'));
 });
